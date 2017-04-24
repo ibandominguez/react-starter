b0b1969a01f9c81628f704f7f5a5b22264efa42c
ibandominguez <ibandominguez@live.com>
2017-04-24 14:43:12 +0100

    redux setup

--- src/app.js
+++ src/app.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import { Router, Route, hashHistory } from 'react-router'
-import Hello from './components/Hello'
+import TestContainer from './containers/TestContainer'
+import store from './store'
 
 class App extends Component {
   render() {
     return (
-      <Router history={hashHistory}>
-        <Route path='/' component={Hello} />
-      </Router>
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path='/' component={TestContainer} />
+        </Router>
+      </Provider>
     )
   }
 }
---
+++ src/components/TestComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TestComponent extends Component {
+  render() {
+    return (
+      <h3>TestComponent</h3>
+    )
+  }
+}
