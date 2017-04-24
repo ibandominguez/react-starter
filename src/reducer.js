241948bd0d20c2f7137e31394cffd09424372baf
ibandominguez <ibandominguez@live.com>
2017-04-24 14:59:38 +0100

    updated naming to follow convetions

--- src/reducer.js
+++ src/reducer.js
@@ -1,14 +1,12 @@
-import { createStore } from 'redux'
-
 const initialState = {
   test: 'test'
 }
 
-export default createStore((state = initialState, action) => {
+export default (state = initialState, action) => {
   switch (action.type) {
     case 'ACTION':
       return state
     default:
       return state
   }
-})
+}
