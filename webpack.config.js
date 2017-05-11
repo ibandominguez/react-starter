ba5c46b2be78d23a50ced56f17dfd589ad799f9f
ibandominguez <ibandominguez@live.com>
2017-05-11 04:02:02 +0100

    html-webpack plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,26 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
     main: './src/app.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     publicPath: '/',
-    filename: 'app.min.js',
+    filename: 'scripts/app.min.js',
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.(png|jpg|gif|svg|woff|svg|ttf|eot)$/,
-      loader: 'url?limit=30000&name=[name]-[hash].[ext]'
-    }, {
-        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
-        loader: 'url-loader'
-    }, {
-      test: /\.less$/,
-      exclude: '/node_modules',
-      loader: 'style!css!less'
-    }]
+    loaders: [
+      { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.(png|jpg|gif)$/, loader: 'url?limit=30000&name=images/[name].[ext]' },
+      { test: /\.(woff|svg|ttf|eot)$/, loader: 'url?limit=30000&name=fonts/[name].[ext]' },
+      { test: /\.(pdf|doc)$/, loader: 'url?limit=30000&name=documents/[name].[ext]' },
+      { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, loader: 'url-loader' },
+      { test: /\.less$/, exclude: '/node_modules', loader: 'style!css!less' }
+    ]
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -37,6 +31,18 @@ module.exports = {
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: { warnings: false }
+    }),
+    new HtmlWebpackPlugin({
+      title: 'React Starter',
+      template: require('html-webpack-template'),
+      appMountId: 'app',
+      // baseHref: '/',
+      // googleAnalytics: { trackingId: 'UA-XXXX-XX', pageViewOnLoad: true },
+      meta: [
+        { charset: 'utf-8' },
+        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ]
     })
   ]
 }
