de00703c3cdbadddc7028b2e7d6061d89a873940
ibandominguez <ibandominguez@live.com>
2017-05-27 20:26:09 +0100

    test component cleaning up

--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -9,11 +9,26 @@ export default class TestComponent extends Component {
     }
   }
 
+  get styles() {
+    return {
+      container: Object.assign({}, this.props.style),
+      image: {
+        maxWidth: '100%',
+        width: '100%'
+      }
+    }
+  }
+
+  getFontColor() {
+    const color = this.props.test ? 'green' : 'red'
+    return { color }
+  }
+
   render() {
     return (
-      <div style={this.props.style} onClick={this.props.onComponentClick}>
-        <h3>TestComponent <span style={{ color: this.props.test ? 'green' : 'red' }}>TEST BOOLEAN</span></h3>
-        <img style={{ maxWidth: '100%', width: '100%' }} src={require('../images/test.png')} />
+      <div style={this.styles.container} onClick={this.props.onComponentClick}>
+        <h3>TestComponent <span style={this.getFontColor()}>TEST BOOLEAN</span></h3>
+        <img style={this.styles.image} src={require('../images/test.png')} />
       </div>
     )
   }
