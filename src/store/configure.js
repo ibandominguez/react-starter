7e3546690ce50346b42d0f21bf4b259135acb438
Mario Peñate Fariñas <ckwanted@gmail.com>
2017-06-27 00:46:19 +0100

    Redux logger only development

--- src/store/configure.js
+++ src/store/configure.js
@@ -2,9 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import rootReducer from '../reducers'
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware
-)(createStore)
+const middlewares = [thunkMiddleware]
+
+if (process.env.NODE_ENV === 'development') {
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
 
 const configureStore = (initialState = {}) => {
   return createStoreWithMiddleware(rootReducer, initialState)
