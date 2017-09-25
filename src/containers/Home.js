d8cba3d580e6bc21c1a81027f4cbc325ae3c29e9
ibandominguez <ibandominguez@live.com>
2017-09-25 18:15:15 +0100

    remove bs

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,17 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { CardBox, AddCard } from '../components'
-import { Grid, Row, Col, Panel } from 'react-bootstrap'
-
-const styles = {
-  cards: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    flexWrap: 'wrap',
-    alignItems: 'center'
-  }
-}
 
 class Home extends Component {
   addCard(card) {
@@ -25,23 +14,15 @@ class Home extends Component {
 
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col xs={10} xsOffset={1}>
-            <Panel header='Home Container'>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <div>
+        <AddCard onAdd={this.addCard.bind(this)} />
 
-              <AddCard onAdd={this.addCard.bind(this)} />
-
-              <div style={styles.cards}>
-                {this.props.cards.map((card, index) => (
-                  <CardBox key={index} {...card} onClick={console.log} />
-                ))}
-              </div>
-            </Panel>
-          </Col>
-        </Row>
-      </Grid>
+        <div>
+          {this.props.cards.map((card, index) => (
+            <CardBox key={index} {...card} onClick={console.log} />
+          ))}
+        </div>
+      </div>
     )
   }
 }
