b5043ed2e82e505e701107f24506f3740eee2c76
ibandominguez <domingueznoda@gmail.com>
2018-07-28 08:45:02 +0100

    added webpack clean and renamed entry point file

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,16 @@ const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    main: './src/app.js'
+    main: './src/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: '/',
-    filename: 'scripts/[hash].js'
+    filename: 'scripts/[hash].min.js'
   },
   module: {
     rules: [
@@ -27,6 +28,7 @@ module.exports = {
   },
   optimization: {},
   plugins: [
+    new CleanWebpackPlugin('build', {}),
     new HtmlWebpackPlugin({
       title: 'React Starter',
       inject: false,
